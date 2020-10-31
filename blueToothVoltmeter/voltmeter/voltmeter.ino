6a68636a8812be54cb2736cc722ec9d17bb36f89
Rosealuis <alois.rosecky@gmail.com>
2020-10-31 23:31:06 +0100

    přejmenování proměnných

--- blueToothVoltmeter/voltmeter/voltmeter.ino
+++ blueToothVoltmeter/voltmeter/voltmeter.ino
@@ -5,24 +5,24 @@
 
 // na hornim PC je to arduino NANO na COM6
 
-const int analogInput1ch = 0;
-const int analogInput2ch = 0;
+const int analogInput_1ch = 0;
+const int analogInput_2ch = 0;
 
-float vout1ch = 0.0;
-float vin1ch = 0.0;
-float R11ch = 98800.0;
-float R21ch = 9920.0;
-int value1ch = 0;
+float vout_1ch = 0.0;      //z bit. prevedene napeti 1. kanalu 
+float vin_1ch = 0.0;       //spocitane napeti 1. kanalu z delice napeti
+float R1_1ch = 98800.0;    //hodnota 100kOhm odporu delice napeti
+float R2_1ch = 9920.0;     //hodnota 10kOhm odporu delice napeti
+int value_1ch = 0;         //bit. hodnota napeti z analogoveho vstupu arduino z 1. kanalu
 
-unsigned long previousMillis = 0;       // will store last time LED was updated
-const long interval = 1000; 
-int sekundy = 0;
+unsigned long previousMillis = 0;       
+const long interval = 100; 
+int setiny = 0;
 
 
 
 void setup() {
   Serial.begin(115200);
-  pinMode(analogInput1ch, INPUT);
+  pinMode(analogInput_1ch, INPUT);
 }
 
 
@@ -30,17 +30,22 @@ void loop() {
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
-    sekundy += 1;
+    setiny += 1;
   }
+
+  if (setiny == 10) {
+
+  }
+    
+    value_1ch = analogRead(analogInput_1ch);
+    vout_1ch = (value_1ch * 5.0) / 1024.0;
+    vin_1ch = vout_1ch / (R2_1ch/(R1_1ch+R2_1ch));
+    if (vin_1ch < 0.09) {
+      vin_1ch = 0.0;
+    }
+    Serial.println(vin_1ch);
+    setiny = 0;
   
-  value1ch = analogRead(analogInput1ch);
-  vout1ch = (value1ch * 5.0) / 1024.0;
-  vin1ch = vout1ch / (R21ch/(R11ch+R21ch));
-  if (vin1ch < 0.09) {
-    vin1ch = 0.0;
-  }
-
-  Serial.println(vin1ch);
-
+  
   
 }
