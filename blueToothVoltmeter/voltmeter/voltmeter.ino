9ffed6e3bd1e113f9fd80899818cd09a41b5dc25
Rosealuis <alois.rosecky@gmail.com>
2020-11-01 00:13:09 +0100

    přidán offset pro kalibraci s multimetrem

--- blueToothVoltmeter/voltmeter/voltmeter.ino
+++ blueToothVoltmeter/voltmeter/voltmeter.ino
@@ -10,16 +10,15 @@ const int analogInput_2ch = 1;      //cislo analogoveho vstupu 2. kanalu
 
 float vout_1ch = 0.0;               //z bit. prevedene napeti 1. kanalu 
 float vin_1ch = 0.0;                //spocitane napeti 1. kanalu z delice napeti
-float R1_1ch = 98800.0;             //hodnota 100kOhm odporu delice napeti
-float R2_1ch = 9920.0;              //hodnota 10kOhm odporu delice napeti
+float R1_1ch = 98800.0;             //hodnota 100kOhm odporu delice napeti 1. kanalu
+float R2_1ch = 9920.0;              //hodnota 10kOhm odporu delice napeti 1. kanalu
 int value_1ch = 0;                  //bit. hodnota napeti z analogoveho vstupu arduino z 1. kanalu
+float offset_1ch = -0.21;           //offset 1. kanalu dany merenim napeti multimetrem
 
-unsigned long previousMillis = 0;       
-const long interval = 1;          //chci 100ms cyklus mereni
-int setiny = 0;
+unsigned long previousMillis = 0;   //promenna pro cas
+const long interval = 1;            //promenna pro cas
+int setiny = 0;                     //promenna pro cas
 
-int statusSetina = 0;                 //pomocna promena pro pulz 100ms
-int statusSekunda = 0;                //pomocna promena pro pulz 1s
 
 
 
@@ -33,9 +32,11 @@ void readChannel1() {
   value_1ch = analogRead(analogInput_1ch);
   vout_1ch = (value_1ch * 5.0) / 1024.0;
   vin_1ch = vout_1ch / (R2_1ch/(R1_1ch+R2_1ch));
-  if (vin_1ch < 0.09) {
+  /*if (vin_1ch < 0.09) {
     vin_1ch = 0.0;
-  }
+  }*/
+  vin_1ch += offset_1ch;
+  
   Serial.println(vin_1ch);
 }
 
